a6a46c84386a7a1d58dac54efa68f03324dc114a
MAtt5816 <m.szewczyk58@gmail.com>
2021-12-24 16:14:35 +0100

    Added Menu class

---
+++ classes/Menu.php
@@ -0,0 +1,19 @@
+<?php
+  class Menu
+  {
+    private category;
+
+    public function __construct($category)
+    {
+      $this->category = $category;
+    }
+
+    public print()
+    {
+      //TODO... printing menu from DB
+    }
+
+    public __destruct()
+    {}
+  }
+?>
