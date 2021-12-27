9806609751b4c7d59aaeaf20b57054955849a5ca
MAtt5816 <m.szewczyk58@gmail.com>
2021-12-27 11:53:14 +0100

    Added build_table() method to the Menu class

--- classes/Menu.php
+++ classes/Menu.php
@@ -1,11 +1,23 @@
 <?php
   class Menu
   {
-    private category;
+    private $category;
+
+    private row_start, row_end, cell_start, cell_end = '<tr>', '</tr>', '<td>', '</td>';
+
+    protected $categories_types = array('Dania główne' => 'glowne', 'Dodatki obiadowe do wyboru' => 'dodatki', 'Zupy' => 'zupy', 'Potrawy mączne' => 'maczne');
 
     public function __construct($category)
     {
       $this->category = $category;
+    }
+
+    public build_table($db){
+      $sql = "SELECT * FROM menu WHERE `kategoria` LIKE $categories_types[$category]";
+      $fields = array('nazwa', 'cena');
+      $result = $db->select($sql, $fileds);
+
+      var_dump($result);
     }
 
     public print()
