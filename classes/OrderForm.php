e4b9598f6544cf85e86e9834e97aea5f34a929e0
MAtt5816 <m.szewczyk58@gmail.com>
2021-12-28 12:38:49 +0100

    Added OrderForm class.

---
+++ classes/OrderForm.php
@@ -0,0 +1,34 @@
+<?php
+  include_once 'Menu.php';
+  
+  class OrderForm extends Menu
+  {
+    private $category;
+
+    function __construct($category)
+    {
+      $this->category = $category;
+    }
+
+    public function print($db)
+    {
+      list($result, $key, $cat) = $Menu->build_table($db);
+      echo "fieldset";
+      $content = "<legend>$cat</legend>";
+      foreach ($result as $value) {
+        $content .= '<input type="checkbox" name="'.$value['name_tag'].'" id="'.$value['name_tag'].'">';
+        $content .= '<div class="menuPosition" id="div_'.$value['name_tag'].'">';
+        $content .= '<label for="'.$value['name_tag'].'"><span class="menuPosition_element dish_name">';
+        $content .= $value['nazwa'].'</span>';
+        $content .= '<span class="menuPosition_element price">';
+        $content .= $value['cena'].' zł</span></label>';
+        $content .= '</div>';
+      }
+      echo $content;
+      echo "</fieldset>";
+    }
+
+    public function __destruct()
+    {}
+  }
+?>
