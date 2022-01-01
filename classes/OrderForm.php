ca96e470b233a67bcdb56e3c2886fae062c2effe
MAtt5816 <m.szewczyk58@gmail.com>
2022-01-01 14:36:00 +0100

    Added insertToDB() method into OrderForm class.

--- classes/OrderForm.php
+++ classes/OrderForm.php
@@ -51,7 +51,78 @@
       echo "</fieldset>";
     }
 
+    function insertToDB($db){
+      $data = $this->validation($this->filter_array);
+      if($data != ""){
+          $positions = "";
+          foreach ($data['position'] as $val){
+              $positions .= $val.",";
+          }
+          $positions = substr_replace($positions, "", -1);
+          $data['position'] = $positions;
 
+          if(isset($data['day']) && $data['hour']){
+              $datatime = $data['day'] + $data['hour'];
+              $data += ['datatime' => $datatime];
+          }
+          elseif (isset($data['day'])) {
+              $data += ['datatime' => $data['day']];
+          }
+          elseif (isset($data['hour'])) {
+              $data += ['datatime' => $data['hour']];
+          }
+          unset($data['date']);
+          unset($data['hour']);
+
+          $string = "";
+          foreach ($data as $val){
+              $string .='"'.$val.'", ';
+          }
+
+          $string = substr_replace($string, "", -2, 2);
+
+          list($type, $order, $name, $surname, $tel, $realisation, $street, $number, $payment, $comment, $realisation_date) = explode(", ", $string);
+
+          $sql = [
+              "INSERT INTO `zamowienie`(`ID`, `user_ID`, `typ`, `daneKlienta_ID`, `czasRealizacji_typ`, `dataRealizacji`, `adres_ID`, `uwagi`, `platnosc`)
+                  VALUES (NULL, 1, $type, 1, $realisation, $realisation_date, 1, $comment, $payment);",        //TODO  // replace static ID
+              "INSERT INTO `lista_pozycji`(`zamowienie_ID`, `menu_ID`, `ilosc`)
+                  VALUES (last_id, 1, 2), (last_id, 5, 3);"
+          ];
+
+          $last_id = 0;
+          $is_exception = false;
+          if($db->transaction()){
+            try{
+              if($db->insert($sql[0])){
+                  $last_id = $db->last_id();
+              }
+              else{
+                  throw new Exception();
+              }
+
+              foreach($sql as $key => $query){
+                  $query = str_replace("last_id", $last_id, $query);
+                  printf($last_id);echo $query;
+                  if ($key !== array_key_first($sql)){
+                    if (!($db->insert($query))){
+                      throw new Exception();
+                    }
+                  }
+              }
+              $db->commit();
+            }
+            catch (Exception | mysqli_sql_exception $exception){
+                $db->rollback();
+                echo "Błąd dodania do bazy";
+                $is_exception = true;
+            }
+          }
+          if(!($is_exception)){
+              echo "Dodano do bazy";
+          }
+      }
+    }
 
     public function __destruct()
     {
