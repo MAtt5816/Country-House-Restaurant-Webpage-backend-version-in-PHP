9366ecbd7a75617ae8cd8d55ade786c2f9516b01
MAtt5816 <m.szewczyk58@gmail.com>
2022-01-22 17:03:42 +0100

    Copyright updated

--- snippets/footer.php
+++ snippets/footer.php
@@ -14,7 +14,7 @@
       <a href="tel:+48225553040"><span class="material-icons">call</span> (+48) 22 555 30 40</a><br>
       <a href="https://www.google.com/maps/place/Kr%C3%B3tka+11,+24-100+Pu%C5%82awy/" target="_blank"><span class="material-icons">location_on</span> ul. Krótka 11 00-001 Miasto</a><br>
     </section>
-    <div><hr>&copy; 2021 Mateusz Szewczyk</div>
+    <div><hr>&copy; 2021-2022 Mateusz Szewczyk</div>
   </footer>
   ';
 ?>
