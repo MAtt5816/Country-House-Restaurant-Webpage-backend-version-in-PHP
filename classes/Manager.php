c6b646485012fb0ff6fc6cc2c90cf3678d3500c7
MAtt5816 <m.szewczyk58@gmail.com>
2022-01-23 12:04:37 +0100

    Added manager buttons actions

--- classes/Manager.php
+++ classes/Manager.php
@@ -17,37 +17,72 @@
       $user = unserialize($_SESSION['userID']);
       $this->userID = $user->userID;
     }
+
+    public function main($db, $uid){
+      $sql = "SELECT `imie` FROM `dane_klienta` WHERE `user_ID` = $uid";
+      $fields = ['imie'];
+
+      try{
+        if($db->select($sql, $fields) != 0){
+            $result = $db->select($sql, $fields);
+        }
+        else{
+            throw new Exception();
+        }
+      }
+      catch (Exception | mysqli_sql_exception $exception){
+          $db->rollback();
+          echo "Błąd dostępu do bazy danych";
+      }
+
+      $name = $result[0]['imie'];
+
+      echo '
+      <div id="welcome">
+        Witaj, '.$name.'!
+      </div>
+      ';
+      ?>
+        <button type="button" name="data" id="data" onclick="location.href='panel.php?form=Konto&card=data'">Twoje dane</button>
+        <button type="button" name="addresses" id="addresses" onclick="location.href='panel.php?form=Konto&card=addresses'">Twoje adresy</button>
+        <button type="button" name="orders" id="orders" onclick="location.href='panel.php?form=Konto&card=orders'">Twoje zamówienia</button>
+      <?php
+    }
+
+    public function data($db, $uid){
+
+    }
+
+    public function addresses($db, $uid){
+
+    }
+
+    public function orders($db, $uid){
+
+    }
+
     public function show($db){
       $uid = $this->userID;
       if($uid > 0){
-        $sql = "SELECT `imie` FROM `dane_klienta` WHERE `user_ID` = $uid";
-        $fields = ['imie'];
-
-        try{
-          if($db->select($sql, $fields) != 0){
-              $result = $db->select($sql, $fields);
-          }
-          else{
-              throw new Exception();
+        if(isset($_GET['card'])){
+          $card = $_GET['card'];
+          switch ($card) {
+            case 'data':
+              $this->data($db, $uid);
+              break;
+            case 'addresses':
+              $this->addresses($db, $uid);
+              break;
+            case 'orders':
+              $this->orders($db, $uid);
+              break;
+            default:
+              break;
           }
         }
-        catch (Exception | mysqli_sql_exception $exception){
-            $db->rollback();
-            echo "Błąd dostępu do bazy danych";
+        else{
+          $this->main($db, $uid);
         }
-
-        $name = $result[0]['imie'];
-
-        echo '
-        <div id="welcome">
-          Witaj, '.$name.'!
-        </div>
-        ';
-        ?>
-          <button type="button" name="data" id="data" onclick="location.href='panel.php?form=Konto&card=data'">Twoje dane</button>
-          <button type="button" name="addresses" id="addresses" onclick="location.href='panel.php?form=Konto&card=addresses'">Twoje adresy</button>
-          <button type="button" name="orders" id="orders" onclick="location.href='panel.php?form=Konto&card=orders'">Twoje zamówienia</button>
-        <?php
       }
     }
   }
